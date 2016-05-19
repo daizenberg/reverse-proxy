ce3d1a5accfa9c8c475f05452878c0c33eec4d2c
Dmitri Aizenberg <dmitri.aiz@gmail.com>
2016-05-19 11:41:49 -0700

    refactoring

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var server = new Http.Server()
 server.listen(1980, '127.0.0.1')
 
 server.on('request', (req, res) => {
-  var url = parseUrl(req.url)
+  var url = proxyUrl.proxyToReal(req.url)
   if (!url) {
     console.error(`Error parsing source URL: ${req.url}`)
     res.end('Error: could not parse request URL')
@@ -14,93 +14,101 @@ server.on('request', (req, res) => {
   }
 
   console.log(`Connecting to ${url.hostname}${url.path}`)
-  fetchUrl(url,
-    res,
+  requestUrl(url,
+    (instream) => finishResponseSuccess(instream, res),
     (error) => finishResponseError(res, error))
 })
 
-function fetchUrl (url, localRes, onError) {
-  Http.get(url, (response) => {
-    var isParsable = parseHeaders(localRes, response)
-    if (isParsable) {
-      let parser = new Parse.SAXParser()
-      let piggy = (text) => text.replace(/(\w)(\w*)/g, '$2$1ay')
-      let direct = (text) => text
-      let textConverter = piggy
-      parser.on('text', (t) => {
-        localRes.write(textConverter(t))
-      })
-      parser.on('startTag', (name, attrs, selfClosing) => {
-        if (selfClosing || name === 'script' || name === 'style') {
-          textConverter = direct
-        } else {
-          textConverter = piggy
-        }
-        localRes.write(`<${name}`)
-        attrs.forEach(a => {
-          if (a.name === 'href') {
-            a.value = convertUrl(a.value)
-          }
-          localRes.write(` ${a.name}="${a.value}"`)
-        })
-        if (selfClosing) {
-          localRes.write('/')
-        }
-        localRes.write('>')
-      })
-      parser.on('endTag', (name) => {
-        localRes.write(`</${name}>`)
-        textConverter = piggy
-      })
-      parser.on('doctype', (name, publicId, systemId) => {
-        localRes.write(`<!DOCTYPE ${name}>`)
-      })
-      parser.on('end', () => localRes.end())
-      response.pipe(parser)
-    } else {
-      response.pipe(localRes)
-    }
-  }).on('error', (e) => onError(e))
+function requestUrl (url, onSuccess, onError) {
+  Http.get(url, (response) => onSuccess(response))
+      .on('error', (e) => onError(e))
 }
 
-function finishResponseError (localRes, error) {
+function finishResponseError (outstream, error) {
   console.error(`Request error: ${error.message}`)
-  localRes.end(`Remote host returned error: ${error.message}`)
+  outstream.statusCode = 500
+  outstream.end(`Request error: ${error.message}`)
 }
 
-function parseHeaders (localRes, remoteRes) {
-  let isParsable = true
-  console.log(`Successfully recieved response, status code ${remoteRes.statusCode}`)
-  localRes.statusCode = remoteRes.statusCode
-  for (let header in remoteRes.headers) {
-    let headerValue = remoteRes.headers[header]
+function finishResponseSuccess (instream, outstream) {
+  var isHtml = translateHeaders(instream, outstream)
+  if (isHtml) {
+    outstream = attachPiggyTransformation(outstream)
+  }
+  instream.pipe(outstream)
+}
+
+function translateHeaders (instream, outstream) {
+  let isHtml = true
+  console.log(`Successfully recieved response, status code ${instream.statusCode}`)
+  outstream.statusCode = instream.statusCode
+  for (let header in instream.headers) {
+    let headerValue = instream.headers[header]
     header = header.toLowerCase()
     if (header === 'location') {
-      headerValue = convertUrl(headerValue)
+      headerValue = proxyUrl.realToProxy(headerValue)
     }
     if (header === 'content-type' && headerValue.indexOf('html') < 0) {
-      isParsable = false
+      isHtml = false
     }
-    localRes.setHeader(header, headerValue)
+    outstream.setHeader(header, headerValue)
   }
-  return isParsable
+  return isHtml
 }
 
-function parseUrl (sourceUrl) {
-  var url = Url.parse(sourceUrl)
-  var matches = /host=((w|.|-|_)+)/.exec(url.query)
-  if (!matches || matches.length < 1) {
-    return null
-  }
-  return {
-    hostname: matches[1],
-    path: url.pathname
-  }
+function attachPiggyTransformation (outstream) {
+  let parser = new Parse.SAXParser()
+  let piggy = (text) => text.replace(/(\w)(\w*)/g, '$2$1ay')
+  let direct = (text) => text
+  let textConverter = piggy
+  parser.on('text', (t) => {
+    outstream.write(textConverter(t))
+  })
+  parser.on('startTag', (name, attrs, selfClosing) => {
+    if (selfClosing || name === 'script' || name === 'style') {
+      textConverter = direct
+    } else {
+      textConverter = piggy
+    }
+    outstream.write(`<${name}`)
+    attrs.forEach(a => {
+      if (a.name === 'href') {
+        a.value = proxyUrl.realToProxy(a.value)
+      }
+      outstream.write(` ${a.name}="${a.value}"`)
+    })
+    if (selfClosing) {
+      outstream.write('/')
+    }
+    outstream.write('>')
+  })
+  parser.on('endTag', function (name) {
+    outstream.write(`</${name}>`)
+    textConverter = piggy
+  })
+  parser.on('doctype', function (name) {
+    outstream.write(`<!DOCTYPE ${name}>`)
+  })
+  parser.on('end', () => outstream.end())
+  return parser
 }
 
-function convertUrl (sourceUrl) {
-  var url = Url.parse(sourceUrl)
-  var x = `http://localhost:1980${url.pathname}?host=${url.hostname}`
-  console.log(`converted url ${sourceUrl} to ${x}`)
-  return x
+const proxyUrl = {
+  proxyToReal: function (proxyUrl) {
+    var url = Url.parse(proxyUrl)
+    var matches = /host=((w|.|-|_)+)/.exec(url.query)
+    if (!matches || matches.length < 1) {
+      return null
+    }
+    return {
+      hostname: matches[1],
+      path: url.pathname
+    }
+  },
+  realToProxy: function (realUrl) {
+    var url = Url.parse(realUrl)
+    var x = `http://localhost:1980${url.pathname}?host=${url.hostname}`
+    console.log(`converted url ${realUrl} to ${x}`)
+    return x
+  }
 }
